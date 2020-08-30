6ebdfaa008b64fa1fface6bd075f8b6e541dfecb
Connor Prettyman <connor.prettyman@gmail.com>
2020-08-30 19:20:15 +0100

    Modify Setup

--- setup.sh
+++ setup.sh
@@ -2,7 +2,18 @@
 
 # Install Script for Configuration Files
 
+cd $HOME
+
 echo Cloning Github Repository...
 
 git clone https://github.com/Conjuam/dotfiles Conjuam-Dotfiles
 
+echo Copying Files To Their Locations...
+
+cp -rviT Conjuam-Dotfiles $HOME
+
+echo Cleaning Up...
+
+rm -rf Conjuam-Dotfiles
+
+echo Done! Log Out or Reboot to ensure changes are successful!
